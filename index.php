b83d4e2d3db94b5d00e3f6513cbf112dbac8ad4c
hyde2 <jeffgrogan64@gmail.com>
2011-10-04 18:26:18 -0700

    initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+
+Hello World<br>
+How have you been?<br>
+<?php echo "Im fine<br>" ?>
+
+</body>
+</html>
